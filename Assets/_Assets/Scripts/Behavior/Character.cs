3cd7846e98df110477ab9b25778364aba7297e95
Minh Duc <DucNLM.BI12-092@st.usth.edu.vn>
2025-08-26 14:30:45 +0700

    Add movement to character

---
+++ Assets/_Assets/Scripts/Behavior/Character.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [Header("Movement settings")] 
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    
+    [Header("Ground Check")]
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius;
+    
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private float moveInput;
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        moveInput = 0f;
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveInput = -1f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveInput = 1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+    }
+}
